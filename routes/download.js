4d52534d262a954ff5491d5f4dab71646a939bf7
Darien Opperman <darien@taklop.io>
2017-03-11 21:59:16 +0000

    Minor Readability Changes
    Changed some braces to space out more

--- routes/download.js
+++ routes/download.js
@@ -3,14 +3,17 @@ var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) 
+{
   res.render('download', { title: 'download' });
 });
 
-router.get('/pdf', function(req, res, next) {
+router.get('/pdf', function(req, res, next) 
+{
 	res.download('files/pdfSummary.pdf', 'pdfSummary.pdf');
 });
-router.get('/word', function(req, res, next) {
+router.get('/word', function(req, res, next) 
+{
 	res.download('files/wordSummary.docx', 'wordSummary.docx');
 });
 
