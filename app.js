a0350301b2d4fc43f94575e9aaa0422d758296cd
SamLubbers <lubbers.sc@gmail.com>
2017-03-13 22:22:11 +0000

    made design for download page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var download = require('./routes/download');
 var upload = require('./routes/upload');
 
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/download', download);
 app.use('/upload', upload);
 
--- public/javascripts/download.js
+++
@@ -1,9 +0,0 @@
-$("#pdf_download").click(function ()
-{
-	console.log("pdf wants to download");
-	window.open('/pdf');
-});
-$("#word_download").click(function ()
-{
-	window.open('/word');
-});
