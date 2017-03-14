481e961d3c08709d585d4195a94722f9859a949a
Darien Opperman <darien@taklop.io>
2017-03-14 13:08:21 +0000

    Pushing the file_upload attempts

--- routes/file_upload.js
+++ routes/file_upload.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HCI prototype' });
+router.get('/', function(req, res, next) 
+{
+  res.render('file_upload', { title: 'File Upload Redirect test' });
 });
 
 module.exports = router;
