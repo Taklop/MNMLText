601767fad228fc487d5cf0413eb43cb422c639b4
SamLubbers <lubbers.sc@gmail.com>
2017-04-02 21:26:19 +0100

    final prototype done, just need to make new summary

--- routes/download.js
+++ routes/download.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
-  res.render('download', { title: 'download' });
+  res.render('download', { title: 'Download' });
 });
 
 router.get('/pdf', function(req, res, next)
